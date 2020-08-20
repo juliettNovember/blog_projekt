02f486cdcad4088222f6e950e525f05db9eefb4a
juliettNovember <nowakowska.just@gmail.com>
2020-08-20 12:00:27 +0200

    First step

--- __init__.py
+++ __init__.py
@@ -2,10 +2,18 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from blog.models import Entry, db
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+@app.shell_context_processor
+def make_shell_context():
+  return {
+      "db": db,
+      "Entry": models.Entry
+  }
 
 from blog import routes, models
